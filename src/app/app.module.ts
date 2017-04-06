dde65eac53c3ee4c17dac63c64bcf00529c284f1
vansoncrasta <vansoncrasta@gmail.com>
2017-04-06 20:42:18 +0530

    Upgrade to Ionic 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 //Ionic Native
@@ -21,6 +23,8 @@ import { EPaperService, UserSettingsService } from '../providers/index';
     EPapersPage
   ],
   imports: [
+    BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
     IonicStorageModule.forRoot()
   ],
